cee0160009661149a223bb54882b91979484b654
RaresPopa22 <poparares1992@gmail.com>
2025-09-01 23:43:55 +0300

    wip

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,10 @@ import argparse
 import os
 
 import joblib
-import numpy as np
 import pandas as pd
 import yaml
 from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, roc_auc_score, roc_curve
 
 
 def load_dataset(config):
@@ -31,7 +31,7 @@ def parse_args_and_get_config(stage):
     parser = argparse.ArgumentParser()
 
     if stage == 'train':
-        parser.add_argument('--config', required=True,  help='Path to configuration file')
+        parser.add_argument('--config', required=True, help='Path to configuration file')
         args = parser.parse_args()
         return read_configs(base_config, args.config)
     elif stage == 'evaluate':
@@ -52,14 +52,14 @@ def save_model(config, model):
 
 def save_test_data(config, X_test, y_test):
     data_config = config['data_paths']
-    np.save(data_config['X_test_npy'], X_test)
-    np.save(data_config['y_test_npy'], y_test)
+    X_test.to_csv(data_config['X_test'], index=False)
+    y_test.to_csv(data_config['y_test'], index=False)
 
 
 def load_test_data(config):
     data_config = config['data_paths']
-    X_test = np.load(data_config['X_test_npy'])
-    y_test = np.load(data_config['y_test_npy'])
+    X_test = pd.read_csv(data_config['X_test'])
+    y_test = pd.read_csv(data_config['y_test'])
 
     return X_test, y_test.squeeze()
 
@@ -78,4 +78,27 @@ def plot_precision_recall_curve(recalls, precisions, labels, auprcs):
     ax.set_ylabel('Precission', fontsize=14)
     ax.set_title('Precision-Recall Curve for Customer Churn', fontsize=16)
     ax.legend(loc='lower left')
+    plt.show()
+
+
+def plot_confusion_matrix(y_test, y_pred, model_name):
+    cm = confusion_matrix(y_test, y_pred)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot()
+    plt.title(f'Confusion Matrix for {model_name}')
+    plt.show()
+
+
+def plot_roc(y_test, y_pred_probas, labels):
+    fig, ax = plt.subplots(figsize=(10, 7))
+
+    for i in range(len(y_pred_probas)):
+        fpr, tpr, _ = roc_curve(y_test, y_pred_probas[i])
+        auc = roc_auc_score(y_test, y_pred_probas[i])
+        ax.plot(fpr, tpr, label=f'{labels[i]} (AUC = {auc:.3f})')
+
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.title('ROC Curve')
+    plt.legend(loc=4)
     plt.show()
