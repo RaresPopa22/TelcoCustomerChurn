ddbc812fc51055ecf18da4178d8233a064db8c96
RaresPopa22 <poparares1992@gmail.com>
2026-02-22 22:14:21 +0200

    important refactor

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,51 +1,69 @@
-from pathlib import Path
-
+import json
+import logging
 import pandas as pd
 from sklearn.metrics import precision_recall_curve, average_precision_score, classification_report, roc_auc_score, \
-    f1_score
+    f1_score, recall_score, precision_score
 
-from src.utils import parse_args_and_get_config, load_test_data, load_model, plot_precision_recall_curve
+from sklearn.calibration import calibration_curve
+
+from src.utils import parse_args_and_get_config, load_test_data, load_pipeline, plot_precision_recall_curve, plot_calibration_curve, suppress_arithmetic_noise
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-def evaluate(config, model_paths):
-    X_test, y_test = load_test_data(config)
+def evaluate(configs):
+    X_test, y_true = load_test_data(configs[0])
 
     results, precisions, recalls, labels, auprcs = [], [], [], [], []
+    cal_probs_true, cal_probs_pred = [], []
 
-    for path in model_paths:
-        model_path = Path(path)
-        model = load_model(model_path)
+    for config in configs:
+        model_path = config['model_output_paths']['model']
+        model = load_pipeline(model_path)
 
-        y_pred = model.predict(X_test)
-        y_pred_proba = model.predict_proba(X_test)[:, 1]
-        precision, recall, _ = precision_recall_curve(y_test, y_pred_proba)
-        auprc = average_precision_score(y_test, y_pred_proba)
+        with suppress_arithmetic_noise():
+            y_pred_proba = model.predict_proba(X_test)[:, 1]
 
-        report = classification_report(y_test, y_pred, target_names=['Non-Churn', 'Churn'])
-        print(f'Classification report for {model_path.stem}')
-        print(report)
+        threshold_path = config['model_output_paths']['threshold']
+        with open(threshold_path, 'r') as f:
+            threshold = json.load(f).get('threshold')
+             
+        y_pred = (y_pred_proba >= threshold).astype(int)
+        precision, recall, _ = precision_recall_curve(y_true, y_pred_proba)
+        auprc = average_precision_score(y_true, y_pred_proba)
+
+        prob_true, prob_pred = calibration_curve(y_true, y_pred_proba, n_bins=10)
+        cal_probs_true.append(prob_true)
+        cal_probs_pred.append(prob_pred)
+
+        report = classification_report(y_true, y_pred, target_names=['Non-Churn', 'Churn'])
+        logger.info(f"Classification report for {config['model_name']}")
+        logger.info(report)
 
         recalls.append(recall)
         precisions.append(precision)
-        labels.append(model_path.stem)
+        labels.append(config['model_name'])
         auprcs.append(auprc)
 
         results.append({
-            'Model': model_path.stem,
+            'Model': config['model_name'],
             'AUPRC': auprc,
-            'ROC AUC': roc_auc_score(y_test, y_pred_proba),
-            'Recall (Churn)': recall,
-            'Precision (Churn)': precision,
-            'F1-Score (Churn)': f1_score(y_test, y_pred, pos_label=1)
+            'ROC AUC': roc_auc_score(y_true, y_pred_proba),
+            'Recall (Churn)': recall_score(y_true, y_pred, pos_label=1),
+            'Precision (Churn)': precision_score(y_true, y_pred, pos_label=1),
+            'F1-Score (Churn)': f1_score(y_true, y_pred, pos_label=1)
         })
 
     results_df = pd.DataFrame(results).set_index('Model')
     pd.set_option('display.max_columns', None)
-    print(results_df.round(4))
+    logger.info(results_df.round(4))
 
     plot_precision_recall_curve(recalls, precisions, labels, auprcs)
+    plot_calibration_curve(cal_probs_true, cal_probs_pred, labels)
+
 
 
 if __name__ == '__main__':
-    config, model_paths = parse_args_and_get_config('evaluate')
-    evaluate(config, model_paths)
+    configs = parse_args_and_get_config('evaluate')
+    evaluate(configs)
